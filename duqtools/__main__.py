97cf060b7fd9942672a8185a0908da03330d0de1
Victor Azizi <victor@lipsum.eu>
2022-05-16 13:36:55 +0200

    added stub configuration object

--- duqtools/__main__.py
+++ duqtools/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from duqtools.config import Config
 
 def parse():
 
@@ -7,11 +8,18 @@ def parse():
   # Globally required options
   parser.add_argument('CONFIG', type=str, help='path to store run files')
 
+  # Subparsers
   subparsers = parser.add_subparsers()
   parser_create  = subparsers.add_parser('create' , help='Create the UQ run files')
   parser_submit  = subparsers.add_parser('submit' , help='Submit the UQ runs')
   parser_analyze = subparsers.add_parser('analyze', help='Analyze the results and generate a report')
   args = parser.parse_args()
+
+
+  # Load the config file
+  global config
+  config = Config(args.CONFIG)
+
 
 if __name__ == '__main__':
   parse()
