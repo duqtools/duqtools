0c30d8daec8647c939b5a6f73daf44e12c3d1a40
Stef Smeets <s.smeets@esciencecenter.nl>
2022-05-16 16:50:03 +0200

    Formatting

--- duqtools/io.py
+++ duqtools/io.py
@@ -3,19 +3,21 @@ import imas
 
 def fetch_ids_data(
     *,
-    shot : int,
-    run : int,
-    user_name : str,
-    db_name : str,
+    shot: int,
+    run: int,
+    user_name: str,
+    db_name: str,
 ) -> imas.DBEntry:
     """Fetch entry from IMAS database.
 
     e.g.
     ```
-    db = fetch_ids_data(shot = 94875, run = 1, user_name = 'g2aho', db_name = 'jet')
+    db = fetch_ids_data(
+        shot = 94875, run = 1, user_name = 'g2aho', db_name = 'jet')
     # -> /afs/eufus.eu/user/g/g2aho/public/imasdb/jet/3/0/ids_948750001.*
-        
-    db = fetch_ids_data(shot=94875, run=250, user_name='g2ssmee', db_name='jet')
+
+    db = fetch_ids_data(
+        shot=94875, run=250, user_name='g2ssmee', db_name='jet')
     # -> /afs/eufus.eu/user/g/g2ssmee/public/imasdb/jet/3/0/ids_948750250.*
     ```
 
@@ -38,11 +40,10 @@ def fetch_ids_data(
     backend = imas.imasdef.MDSPLUS_BACKEND
     db = imas.DBEntry(
         backend_id=backend,
-        shot = shot,
-        run = run,
-        user_name = user_name,
-        db_name = db_name,
+        shot=shot,
+        run=run,
+        user_name=user_name,
+        db_name=db_name,
     )
 
     return db
-
