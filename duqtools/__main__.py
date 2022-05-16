e927d30f227e8a88e0048dfbae5ff6965c0cd732
Victor Azizi <victor@lipsum.eu>
2022-05-16 13:57:41 +0200

    added pydantic as config parser

--- duqtools/__main__.py
+++ duqtools/__main__.py
@@ -1,8 +1,16 @@
 import argparse
 from duqtools.config import Config
 
-def parse():
+def create():
+  pass
 
+def submit():
+  pass
+
+def analyze():
+  pass
+
+def parse():
   parser = argparse.ArgumentParser()
 
   # Globally required options
@@ -11,14 +19,24 @@ def parse():
   # Subparsers
   subparsers = parser.add_subparsers()
   parser_create  = subparsers.add_parser('create' , help='Create the UQ run files')
-  parser_submit  = subparsers.add_parser('submit' , help='Submit the UQ runs')
-  parser_analyze = subparsers.add_parser('analyze', help='Analyze the results and generate a report')
-  args = parser.parse_args()
+  parser_create.set_defaults(func=create)
 
+  parser_submit  = subparsers.add_parser('submit' , help='Submit the UQ runs')
+  parser_submit.set_defaults(func=submit)
+
+  parser_analyze = subparsers.add_parser('analyze', help='Analyze the results and generate a report')
+  parser_analyze.set_defaults(func=analyze)
+
+  # parse the arguments
+  args = parser.parse_args()
+  print("Arguments after parsing: ", args)
 
   # Load the config file
   global config
   config = Config(args.CONFIG)
+
+  # Run the subcommand
+  args.func()
 
 
 if __name__ == '__main__':
--- duqtools/config.py
+++ duqtools/config.py
@@ -1,3 +1,11 @@
-class Config():
+from pydantic import BaseModel
+import yaml
+
+class Config(BaseModel):
+  test: str
+
   def __init__(self, filename):
-    pass
+    with open(filename,'r') as f:
+      datamap = yaml.safe_load(f)
+      print(datamap)
+      BaseModel.__init__(self,**datamap)
