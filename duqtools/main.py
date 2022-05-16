3cb644584d06af5028f4838b2c60bb9b23f6378e
Victor Azizi <victor@lipsum.eu>
2022-05-16 12:18:12 +0200

    Add stub main.py with argparse

---
+++ duqtools/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+
+  # Globally required options
+  parser.add_argument('CONFIG', type=str, help='path to store run files')
+
+  subparsers = parser.add_subparsers()
+  parser_create  = subparsers.add_parser('create' , help='Create the UQ run files')
+  parser_submit  = subparsers.add_parser('submit' , help='Submit the UQ runs')
+  parser_analyze = subparsers.add_parser('analyze', help='Analyze the results and generate a report')
+  args = parser.parse_args()
