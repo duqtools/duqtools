940a1ffb5d0a521367ce6198e1f709d561b9ad57
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-06-03 16:54:16 +0200

    Interface jintrac python config vars (#35)
    * Interface jintrac python config vars
    * Set jintrac config vars programmatically
    * Add tests for jetto settings
    * Make imas an optional install for now

--- duqtools/jetto/_jset.py
+++ duqtools/jetto/_jset.py
@@ -1,4 +1,5 @@
 """Functions to interface with `jetto.jset` files."""
+from __future__ import annotations
 
 from typing import Dict, List, TextIO, Tuple
 
@@ -7,6 +8,69 @@ from .._types import PathLike
 HEADER = """!================================================================
 !                      JETTO SETTINGS FILE
 !================================================================"""
+
+JINTRAC_CONFIG_VARS = (
+    {
+        'name': 'shot_in',
+        'type': int,
+        'key': 'SetUpPanel.idsIMASDBShot',
+        'doc': 'Input IDS shot'
+    },
+    {
+        'name': 'shot_out',
+        'type': int,
+        'key': 'SetUpPanel.shotNum',
+        'doc': 'Output IDS shot'
+    },
+    {
+        'name': 'run_in',
+        'type': int,
+        'key': 'SetUpPanel.idsIMASDBRunid',
+        'doc': 'Input IDS run'
+    },
+    {
+        'name': 'run_out',
+        'type': int,
+        'key': 'JobProcessingPanel.idsRunid',
+        'doc': 'Output IDS run'
+    },
+    {
+        'name': 'user_in',
+        'type': str,
+        'key': 'SetUpPanel.idsIMASDBUser',
+        'doc': 'Input IDS user'
+    },
+    {
+        'name': 'machine_in',
+        'type': str,
+        'key': 'SetUpPanel.idsIMASDBMachine',
+        'doc': 'Input IDS machine'
+    },
+    {
+        'name': 'machine_out',
+        'type': str,
+        'key': 'SetUpPanel.machine',
+        'doc': 'Output IDS machine'
+    },
+    {
+        'name': 'noprocessors',
+        'type': str,
+        'key': 'JobProcessingPanel.numProcessors',
+        'doc': 'JobProcessingPanel.numProcessors'
+    },
+    {
+        'name': 'tstart',
+        'type': float,
+        'key': 'SetUpPanel.startTime',
+        'doc': 'Start time'
+    },
+    {
+        'name': 'tend',
+        'type': float,
+        'key': 'SetUpPanel.endTime',
+        'doc': 'End time'
+    },
+)
 
 
 def parse_section(section: List[str]) -> Tuple[str, Dict[str, str]]:
@@ -114,3 +178,77 @@ def write_jset(path: PathLike, settings: Dict[str, Dict[str, str]]):
 
     with open(path, 'w') as f:
         f.writelines(lines)
+
+
+class JettoSettings:
+
+    def __init__(self, mapping: Dict[str, Dict[str, str]]):
+        self.raw_mapping = mapping
+
+    @property
+    def metadata(self):
+        return self.raw_mapping['File Details']
+
+    @property
+    def settings(self):
+        return self.raw_mapping['Settings']
+
+    def __new__(cls, *args, **kwargs):
+
+        def setter(jset_key: str, jset_type):
+
+            def f(self, value):
+                self.settings[jset_key] = str(value)
+
+            return f
+
+        def getter(jset_key: str, jset_type):
+
+            def f(self):
+                return jset_type(self.settings[jset_key])
+
+            return f
+
+        for variable in JINTRAC_CONFIG_VARS:
+            jset_key = variable['key']
+            jset_type = variable['type']
+            name = variable['name']
+            doc = variable['doc']
+            prop = property(
+                fget=getter(jset_key, jset_type),
+                fset=setter(jset_key, jset_type),
+                doc=doc,
+            )
+            setattr(cls, name, prop)
+
+        return super().__new__(cls)
+
+    @property
+    def components(self):
+        """Active components e.g. EDGE2D, JETTO, HCD (uppercase)"""
+        components = ['JETTO']
+        if self.settings['JobProcessingPanel.selIdsRunid']:
+            components.append('IDSOUT')
+        if self.settings['ExternalWFPanel.select']:
+            components.append('HCD')
+        if self.settings['SetUpPanel.selReadIds']:
+            components.append('IDSIN')
+        return components
+
+    @classmethod
+    def from_file(cls, path: PathLike) -> JettoSettings:
+        """Read JettoSettings from 'jetto.jset' file.
+
+        Parameters
+        ----------
+        path : PathLike
+            Path to `jetto.jset`
+
+        Returns
+        -------
+        jset : JettoSettings
+            Instance of `JettoSettings`
+        """
+        mapping = read_jset(path)
+        jset = cls(mapping)
+        return jset
--- tests/jetto/test_jset.py
+++ tests/jetto/test_jset.py
@@ -1,4 +1,6 @@
-from duqtools.jetto import read_jset, write_jset
+from pathlib import Path
+
+from duqtools.jetto import JettoSettings, read_jset, write_jset
 
 JSET = {
     'section 1': {
@@ -21,3 +23,31 @@ def test_jset_io(tmp_path):
     jset2 = read_jset(path)
 
     assert JSET == jset2
+
+
+def test_JettoSettings(tmp_path):
+    fn = Path(__file__).parent / 'trimmed_jetto.jset'
+    jset = JettoSettings.from_file(fn)
+
+    from duqtools.jetto._jset import JINTRAC_CONFIG_VARS
+
+    assert isinstance(jset.metadata, dict)
+    assert isinstance(jset.settings, dict)
+
+    test_values = {
+        int: 123,
+        float: 3.14,
+        str: 'quack',
+    }
+
+    for var in JINTRAC_CONFIG_VARS:
+        var_type = var['type']
+        var_name = var['name']
+        var_key = var['key']
+        value = getattr(jset, var_name)
+        assert isinstance(value, var_type)
+
+        test_value = test_values[var_type]
+        setattr(jset, var_name, test_value)
+
+        assert jset.settings[var_key] == str(test_value)
