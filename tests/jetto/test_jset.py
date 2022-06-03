f2e39b42556523f3de0b96d66170c2ac8dddb6b7
Stef Smeets <s.smeets@esciencecenter.nl>
2022-06-03 11:08:21 +0200

    Update tests

--- tests/jetto/test_jset.py
+++ tests/jetto/test_jset.py
@@ -1,5 +1,23 @@
-from duqtools.jett import read_jset, write_jset
+from duqtools.jetto import read_jset, write_jset
+
+JSET = {
+    'section 1': {
+        'a': '123',
+        'b': '456'
+    },
+    'section 2': {
+        'c[0]': '1',
+        'c[1]': '2',
+        'c[2]': '3',
+        'd': ''
+    },
+}
 
 
-def test_jset_io():
-    pass
+def test_jset_io(tmp_path):
+    path = tmp_path / 'jetto.jset'
+
+    write_jset(path, JSET)
+    jset2 = read_jset(path)
+
+    assert JSET == jset2
--- tests/jetto/test_namelist.py
+++ tests/jetto/test_namelist.py
@@ -1,15 +1,15 @@
 from duqtools.jetto import patch_namelist, read_namelist, write_namelist
 
-nml = {'nlist1': {'a': 123, 'b': 456}, 'nlist2': {'c': [1, 2, 3], 'd': None}}
+NML = {'nlist1': {'a': 123, 'b': 456}, 'nlist2': {'c': [1, 2, 3], 'd': None}}
 
 
 def test_namelist_io(tmp_path):
     path = tmp_path / 'jetto.in'
 
-    write_namelist(path, nml)
+    write_namelist(path, NML)
     nml2 = read_namelist(path)
 
-    assert nml == nml2
+    assert NML == nml2
 
 
 def test_patch_namelist(tmp_path):
@@ -18,11 +18,11 @@ def test_patch_namelist(tmp_path):
 
     patch = {'nlist1': {'a': 321}, 'nlist3': {'e': 'asdf'}}
 
-    write_namelist(path, nml)
+    write_namelist(path, NML)
     patched = patch_namelist(path, patch, out)
 
     assert out.exists()
-    expected_nlist1 = {**nml['nlist1'], **patch['nlist1']}
+    expected_nlist1 = {**NML['nlist1'], **patch['nlist1']}
     assert patched['nlist1'] == expected_nlist1
-    assert patched['nlist2'] == nml['nlist2']
+    assert patched['nlist2'] == NML['nlist2']
     assert patched['nlist3'] == patch['nlist3']
