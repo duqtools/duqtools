0b2d4143fbda5373421eee0eca28313acf918872
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-06-08 12:16:53 +0200

    Copy all files from template to target directory (#44)
    * Copy all needed files for jetto run
    * Add complete set of jetto input parameters

--- duqtools/create.py
+++ duqtools/create.py
@@ -1,6 +1,8 @@
 import itertools
+import shutil
 from enum import Enum
 from logging import debug
+from pathlib import Path
 from typing import List
 
 from pydantic import BaseModel, DirectoryPath
@@ -35,6 +37,47 @@ class ConfigCreate(BaseModel):
     template: DirectoryPath
 
 
+def copy_files(source_drc: Path, target_drc: Path):
+    """Copy files for jetto run to destination directory.
+
+    Parameters
+    ----------
+    source_drc : Path
+        Source (template) directory.
+    target_drc : Path
+        Target directory.
+    """
+    for filename in (
+            # '.llcmd',
+            'jetto.in',
+            'rjettov',
+            'utils_jetto',
+            'jetto.ex',
+            'jetto.sin',
+            'jetto.sgrid',
+            # 'jetto.jset',
+    ):
+        src = source_drc / filename
+        dst = target_drc / filename
+        shutil.copyfile(src, dst)
+    debug('copied files to %s' % target_drc)
+
+
+def write_batchfile(target_drc: Path):
+    """Write batchfile (`.llcmd`) to start jetto.
+
+    Parameters
+    ----------
+    target_drc : Path
+        Directory to place batch file into.
+    """
+    drc_name = target_drc.name
+    with open(target_drc / '.llcmd', 'w') as f:
+        f.write(f"""#!/bin/sh
+./rjettov -S -I -p -xmpi -x64 {drc_name} v210921_gateway_imas g2fkoech
+""")
+
+
 def create():
     cfg = duqtools.config.Config()
 
@@ -53,19 +96,24 @@ def create():
         for var in combination:
             debug('{source}: {key}={value}'.format(**var))
 
-        subdir = cfg.workspace / f'run_{i:04d}'
-        subdir.mkdir(parents=True, exist_ok=True)
+        sub_drc = f'run_{i:04d}'
+        target_drc = cfg.workspace / sub_drc
+        target_drc.mkdir(parents=True, exist_ok=True)
 
         patch = {
             d['key']: d['value']
             for d in combination if d['source'] == Sources.jetto_jset
         }
         jset_patched = jset.copy_and_patch(settings=patch)
-        jset_patched.to_directory(subdir)
+        jset_patched.to_directory(target_drc)
 
         ids_data = {
             d['key']: d['value']
             for d in combination if d['source'] == Sources.ids
         }
 
-        write_ids(subdir / 'ids.yaml', ids_data)
+        write_ids(target_drc / 'ids.yaml', ids_data)
+
+        copy_files(template_drc, target_drc)
+
+        write_batchfile(target_drc)
