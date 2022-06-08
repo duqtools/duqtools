43f9de266a70c58d44eb4fcb70898ddbc93b4b51
Stef Smeets <s.smeets@esciencecenter.nl>
2022-06-08 08:29:17 +0200

    Add create subcommand

--- duqtools/__main__.py
+++ duqtools/__main__.py
@@ -1,51 +1,63 @@
 import argparse
-from logging import debug
 import logging
-import duqtools.config as cfg
-from duqtools.submit import submit
+from logging import debug
 
-def create():
-  pass
+import duqtools.config as cfg
+
+from .create import create
+from .submit import submit
+
 
 def analyze():
-  pass
+    raise NotImplementedError
+
 
 def cmdline():
-  parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
+    # Subparsers
+    subparsers = parser.add_subparsers()
+    parser_create = subparsers.add_parser('create',
+                                          help='Create the UQ run files')
+    parser_create.set_defaults(func=create)
 
-  # Subparsers
-  subparsers = parser.add_subparsers()
-  parser_create  = subparsers.add_parser('create' , help='Create the UQ run files')
-  parser_create.set_defaults(func=create)
+    parser_submit = subparsers.add_parser('submit', help='Submit the UQ runs')
+    parser_submit.set_defaults(func=submit)
 
-  parser_submit  = subparsers.add_parser('submit' , help='Submit the UQ runs')
-  parser_submit.set_defaults(func=submit)
+    parser_analyze = subparsers.add_parser(
+        'analyze', help='Analyze the results and generate a report')
+    parser_analyze.set_defaults(func=analyze)
 
-  parser_analyze = subparsers.add_parser('analyze', help='Analyze the results and generate a report')
-  parser_analyze.set_defaults(func=analyze)
+    # Globally required options
+    parser.add_argument('CONFIG', type=str, help='path to store run files')
 
-  # Globally required options
-  parser.add_argument('CONFIG', type=str, help='path to store run files')
+    # Global optional options
+    parser.add_argument('--debug',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Enable debug print statements')
+    parser.add_argument('--force',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Force the action you want to take')
 
-  # Global optional options
-  parser.add_argument('--debug', action='store_const', const=True, default=False, help='Enable debug print statements')
-  parser.add_argument('--force', action='store_const', const=True, default=False, help='Force the action you want to take')
+    # parse the arguments
+    args = parser.parse_args()
 
-  # parse the arguments
-  args = parser.parse_args()
+    # Set the debug level
+    if (args.debug):
+        logging.basicConfig(level=logging.INFO)
 
-  # Set the debug level
-  if (args.debug):
-    logging.basicConfig(level=logging.INFO)
+    debug('Arguments after parsing: %s' % args)
 
-  debug("Arguments after parsing: %s"%args)
+    # Load the config file
+    cfg.Config(args.CONFIG)
 
-  # Load the config file
-  cfg.Config(args.CONFIG)
+    # Run the subcommand
+    args.func()
 
-  # Run the subcommand
-  args.func()
 
 if __name__ == '__main__':
-  cmdline()
+    cmdline()
--- duqtools/config.py
+++ duqtools/config.py
@@ -1,35 +1,35 @@
-from typing import Optional
-from pydantic import BaseModel, DirectoryPath
-import yaml
 from logging import debug
+from typing import Optional
+
+import yaml
+from pydantic import BaseModel, DirectoryPath
+
+from duqtools.create import ConfigCreate
 from duqtools.submit import Submit_config
 
+
 class Config(BaseModel):
-  """
-  Config class containing all configs, is a singleton and can be used with
-  import duqtools.config.Config as Cfg
-  Cfg().<variable you want>
-  """
+    """Config class containing all configs, is a singleton and can be used with
+    import duqtools.config.Config as Cfg Cfg().<variable you want>"""
 
-  _instance = None
+    _instance = None
 
-  #pydantic members
-  submit    : Optional[Submit_config]
-  workspace : DirectoryPath
-  force     : bool = False
+    # pydantic members
+    submit: Optional[Submit_config]
+    create: Optional[ConfigCreate]
+    workspace: DirectoryPath
+    force: bool = False
 
-  def __init__(self, filename=None):
-    """
-    Initialize with optional filename argument
-    """
-    if filename:
-      with open(filename,'r') as f:
-        datamap = yaml.safe_load(f)
-        debug(datamap)
-        BaseModel.__init__(self,**datamap)
+    def __init__(self, filename=None):
+        """Initialize with optional filename argument."""
+        if filename:
+            with open(filename, 'r') as f:
+                datamap = yaml.safe_load(f)
+                debug(datamap)
+                BaseModel.__init__(self, **datamap)
 
-  def __new__(cls, *args, **kwargs):
-    # Make it a singleton
-    if not Config._instance:
-      Config._instance = object.__new__(cls)
-    return Config._instance
+    def __new__(cls, *args, **kwargs):
+        # Make it a singleton
+        if not Config._instance:
+            Config._instance = object.__new__(cls)
+        return Config._instance
