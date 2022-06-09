fcf6ef51c481297291634e2a31f69252fb9264ae
Victor Azizi <victor@lipsum.eu>
2022-06-09 11:34:11 +0200

    test_cmdline use context manager + tmpdir fixture

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,21 +1,32 @@
 import os
+import shutil
 import subprocess
+from pathlib import Path
 
 import pytest
 
+from duqtools.utils import work_directory
+
+
+@pytest.fixture(scope='session')
+def cmdline_workdir(tmp_path_factory):
+    workdir = tmp_path_factory.mktemp('test_cmdline')
+    (workdir / Path('workspace')).mkdir()
+    shutil.copy(os.getcwd() + '/example/config.yaml', workdir)
+    shutil.copytree(os.getcwd() + '/example/template_model',
+                    workdir / Path('template_model'))
+    return workdir
+
 
 @pytest.mark.dependency()
-def test_example_create():
-    print(os.getcwd())
-    os.chdir('./example')
-    result = subprocess.run(['duqtools', 'create', 'config.yaml'])
-    assert (result.returncode == 0)
-    os.chdir('../')
+def test_example_create(cmdline_workdir):
+    with work_directory(cmdline_workdir):
+        result = subprocess.run(['duqtools', 'create', 'config.yaml'])
+        assert (result.returncode == 0)
 
 
 @pytest.mark.dependency(depends=['test_example_create'])
-def test_example_status():
-    os.chdir('./example')
-    result = subprocess.run(['duqtools', 'status', 'config.yaml'])
-    assert (result.returncode == 0)
-    os.chdir('../')
+def test_example_status(cmdline_workdir):
+    with work_directory(cmdline_workdir):
+        result = subprocess.run(['duqtools', 'status', 'config.yaml'])
+        assert (result.returncode == 0)
