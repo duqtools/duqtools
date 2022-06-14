566cba963cbba6ab8d255999720db24e1a64f0b6
Victor Azizi <g2vazizi@r054c06s01.galileo.cineca.it>
2022-06-14 12:12:23 +0200

    ids_simplify.py, better structure

--- duqtools/ids/ids_simplify.py
+++ duqtools/ids/ids_simplify.py
@@ -1,40 +1,54 @@
 from collections import defaultdict
 
+import numpy as np
+
 
 def recursive_defaultdict():
     return defaultdict(recursive_defaultdict)
 
 
 class SimpleCoreIDS:
-
-    flat_fields: dict = {}  # All fields in the core profile in a single dict
-    fields: defaultdict = defaultdict(
-        recursive_defaultdict
-    )  # All fields, in the core profile in a nested dict
+    # All fields in the core profile in a single dict
+    flat_fields: dict = {}
+    # All fields, in the core profile in a nested dict
+    fields: defaultdict = defaultdict(recursive_defaultdict)
 
     def __init__(self, ids):
-        self.dive(ids, [ids.__name__])
+        self.dive(ids, [])
 
-    def dive(self, val, path):
-        skip = False
+    def dive(self, val, path: list):
+        """Recursively store the important bits of the imas structure in dicts.
+
+        Parameters
+        ----------
+        val :
+            Current nested object being evaluated
+        path : List
+            Current path
+        """
+
         if isinstance(val, str):
-            skip = True
+            return
 
-        if isinstance(val, list) and not skip:
+        if hasattr(val,
+                   '__getitem__') and not isinstance(val,
+                                                     (np.ndarray, np.generic)):
             for i in range(len(val)):
                 item = val[i]
                 self.dive(item, path + [str(i)])
             return
 
-        if not hasattr(val, '__dict__'):
-            skip = True
-
-        if not skip:
+        if hasattr(val, '__dict__'):
             for key, item in val.__dict__.items():
                 self.dive(item, path + [key])
-        else:
-            self.flat_fields['/'.join(path)] = val
-            cur = self.fields
-            for item in path[:-1]:
-                cur = cur[item]
-            cur[path[-1]] = val
+            return
+
+        if not isinstance(val, (np.ndarray, np.generic)):
+            return
+
+        # We made it here, the value can be stored
+        self.flat_fields['/'.join(path)] = val
+        cur = self.fields
+        for item in path[:-1]:
+            cur = cur[item]
+        cur[path[-1]] = val
