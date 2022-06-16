a1d9591006aecf127781e1a2df27df65e9247841
Victor Azizi <g2vazizi@r054c07s02.galileo.cineca.it>
2022-06-16 14:53:03 +0200

    fixed coverage and added extra test

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -8,8 +8,23 @@ import pytest
 from duqtools.utils import work_directory
 
 
+@pytest.fixture(scope='session', autouse=True)
+def extra_env():
+    # Add required coverage env variable to each test
+    os.environ['COVERAGE_PROCESS_START'] = os.getcwd() + '/setup.cfg'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def collect_cov(cmdline_workdir):
+    yield None
+    # Executed at end of session, but before closure of cmdline_workdir
+    for cov_file in cmdline_workdir.glob('.coverage.*'):
+        shutil.copy(cov_file, os.getcwd())
+
+
 @pytest.fixture(scope='session')
 def cmdline_workdir(tmp_path_factory):
+    # Create working directory for cmdline tests, and set up input files
     workdir = tmp_path_factory.mktemp('test_cmdline')
     (workdir / Path('workspace')).mkdir()
     shutil.copy(os.getcwd() + '/example/config.yaml', workdir)
@@ -30,3 +45,11 @@ def test_example_status(cmdline_workdir):
     with work_directory(cmdline_workdir):
         result = subprocess.run(['duqtools', 'status', 'config.yaml'])
         assert (result.returncode == 0)
+
+
+@pytest.mark.dependency(depends=['test_example_status'])
+def test_example_plot(cmdline_workdir):
+    with work_directory(cmdline_workdir):
+        result = subprocess.run(['duqtools', 'plot', 'config.yaml'])
+        assert (result.returncode == 0)
+        assert (os.path.exists('plot_0000.png'))
