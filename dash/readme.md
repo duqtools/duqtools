c050ddfa06d5115ef12ca076a2f2b951d81ed96f
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-06-17 16:46:51 +0200

    Automatically find and plot data files in workspace (#85)
    * Automatically find and plot data files in workspace
    * Add demo dashboard for plotting
    * Add some documentation for dashboard
    * Use subplot

---
+++ dash/readme.md
@@ -0,0 +1,13 @@
+### Running locally
+
+First, install the requirements:
+
+```console
+pip install -r requirements.txt
+```
+
+To run:
+
+```console
+streamlit run dash.py
+```
--- duqtools/plot.py
+++ duqtools/plot.py
@@ -3,6 +3,9 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 
+from duqtools.ids import ImasLocation
+from duqtools.jetto import JettoSettings
+
 from .config import cfg
 
 logger = logging.getLogger(__name__)
@@ -17,18 +20,25 @@ def plot(**kwargs):
     kwargs :
         kwargs
     """
-
     info('Extracting imas data')
     # Gather all results and put them in a in-memory format
     # (they should be small enough)
     profiles = []
-    for entry in cfg.plot.data:
-        debug('Extracting database: %s' % entry)
-        profiles.append(entry.get_ids_tree('core_profiles'))
+    jset_files = list(cfg.workspace.path.glob('*/*.jset'))
+
+    for jset_file in jset_files:
+        jset = JettoSettings.from_file(jset_file)
+        imas_loc = ImasLocation.from_jset_output(jset)
+        info('Reading %s', imas_loc)
+        profile = imas_loc.get_ids_tree('core_profiles')
+        profiles.append(profile)
 
     for i, plot in enumerate(cfg.plot.plots):
         info('Creating plot number %04i' % i)
-        for profile in profiles:
+
+        fig, ax = plt.subplots()
+
+        for j, profile in enumerate(profiles):
             y = profile.flat_fields[plot.y]
 
             if plot.x:
@@ -36,8 +46,10 @@ def plot(**kwargs):
             else:
                 x = np.linspace(0, 1, len(y))
 
-            plt.xlabel(plot.get_xlabel())
-            plt.ylabel(plot.get_ylabel())
+            ax.set_xlabel(plot.get_xlabel())
+            ax.set_ylabel(plot.get_ylabel())
 
-            plt.plot(x, y)
-        plt.savefig('plot_%04i.png' % i)
+            ax.plot(x, y, label=j)
+
+        ax.legend()
+        fig.savefig('plot_%04i.png' % i)
