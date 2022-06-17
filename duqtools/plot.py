c2f59923fba2beb8c370290da4cab32c31871eef
Stef Smeets <s.smeets@esciencecenter.nl>
2022-06-17 16:46:39 +0200

    Use subplot

--- duqtools/plot.py
+++ duqtools/plot.py
@@ -29,10 +29,15 @@ def plot(**kwargs):
     for jset_file in jset_files:
         jset = JettoSettings.from_file(jset_file)
         imas_loc = ImasLocation.from_jset_output(jset)
-        profiles.append(imas_loc.get_ids_tree('core_profiles'))
+        info('Reading %s', imas_loc)
+        profile = imas_loc.get_ids_tree('core_profiles')
+        profiles.append(profile)
 
     for i, plot in enumerate(cfg.plot.plots):
         info('Creating plot number %04i' % i)
+
+        fig, ax = plt.subplots()
+
         for j, profile in enumerate(profiles):
             y = profile.flat_fields[plot.y]
 
@@ -41,11 +46,10 @@ def plot(**kwargs):
             else:
                 x = np.linspace(0, 1, len(y))
 
-            plt.xlabel(plot.get_xlabel())
-            plt.ylabel(plot.get_ylabel())
+            ax.set_xlabel(plot.get_xlabel())
+            ax.set_ylabel(plot.get_ylabel())
 
-            plt.plot(x, y, label=j)
+            ax.plot(x, y, label=j)
 
-        plt.legend()
-        plt.savefig('plot_%04i.png' % i)
-        plt.close()
+        ax.legend()
+        fig.savefig('plot_%04i.png' % i)
