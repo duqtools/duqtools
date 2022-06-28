3820c6115ca576238e05aa60a6cc30b440fbc37e
Stef Smeets <s.smeets@esciencecenter.nl>
2022-06-28 10:52:22 +0200

    Run streamlit as duqtools command

--- duqtools/dash.py
+++ duqtools/dash.py
@@ -1,2 +1,18 @@
-def dash():
-    print('hello world')
+import logging
+import sys
+from pathlib import Path
+
+from streamlit import cli as stcli
+
+logger = logging.getLogger(__name__)
+
+
+def dash(**kwargs):
+    dashboard_path = Path(__file__).parents[1] / 'dash' / 'dash.py'
+    workdir = Path('.').resolve()
+
+    sys.argv = ['streamlit', 'run', str(dashboard_path), '--', str(workdir)]
+
+    logger.debug('Streamlit arguments %s', sys.argv)
+
+    sys.exit(stcli.main())
