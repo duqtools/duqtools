6b8b6c085a9650ea14d4c4f4ec32b8e7c0634670
Stef Smeets <s.smeets@esciencecenter.nl>
2022-07-11 14:48:10 +0200

    Move attributes from submit->status and add descriptions

--- duqtools/config/status.py
+++ duqtools/config/status.py
@@ -1,9 +1,31 @@
+from pydantic import Field
+
 from .basemodel import BaseModel
 
 
 class StatusConfig(BaseModel):
     """Status_config."""
 
-    msg_completed: str = 'Status : Completed successfully'
-    msg_failed: str = 'Status : Failed'
-    msg_running: str = 'Status : Running'
+    status_file: str = Field('jetto.status',
+                             description='Name of the status file.')
+    in_file: str = Field(
+        'jetto.in',
+        description='Name of the modelling input file, will be used to check'
+        ' if the subprocess has started.')
+    out_file: str = Field(
+        'jetto.out',
+        description='Name of the modelling output file, will be used to check'
+        ' if the subprocess is running,')
+
+    msg_completed: str = Field(
+        'Status : Completed successfully',
+        description='Parse `status_file` for this message to check for'
+        ' completion.')
+    msg_failed: str = Field(
+        'Status : Failed',
+        description='Parse `status_file` for this message to check for'
+        ' failures.')
+    msg_running: str = Field(
+        'Status : Running',
+        description='Parse `status_file` for this message to check for'
+        ' running status.')
