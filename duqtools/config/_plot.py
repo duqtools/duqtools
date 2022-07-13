d84900ef6c90fbc88ab750d031b4c33a692e9fa1
Stef Smeets <s.smeets@esciencecenter.nl>
2022-07-13 09:55:06 +0200

    Update docs for plotting

--- duqtools/config/_plot.py
+++ duqtools/config/_plot.py
@@ -2,17 +2,20 @@ from typing import List, Optional
 
 from pydantic import Field
 
+from ._description_helpers import formatter as f
 from .basemodel import BaseModel
 
 
 class Plot(BaseModel):
     """Description of the plot to produce."""
-    x: Optional[str] = Field(
-        'profiles_1d/0/grid/rho_tor',
-        description='Data on x-axis, default is the toroidal flux coordiante')
+    x: Optional[str] = Field('profiles_1d/0/grid/rho_tor_norm',
+                             description=f("""
+        IDS of the data to plot on the x-axis, default is rho toroidal norm.
+        """))
+
     # TODO, make y axis time-variable by replacing "0" with "*" for example
     y: str = Field('profiles_1d/0/electrons/density_thermal',
-                   description='Data on y-axis')
+                   description='IDS of the data to plot on the y-axis')
 
     xlabel: Optional[str] = Field(None, description='Custom label for x-axis')
     ylabel: Optional[str] = Field(None, description='Custom label for y-axis')
@@ -25,5 +28,19 @@ class Plot(BaseModel):
 
 
 class PlotConfig(BaseModel):
-    """List of plots to make."""
-    plots: List[Plot] = [Plot()]
+    """The options of the plot subcommand are stored under the `plot` key in
+    the config.
+
+    Plots are specified as a list under the `plots` key. Multiple plots
+    can be defined, and they will be written sequentially as .png files
+    to the current working directory.
+    """
+    plots: List[Plot] = [
+        Plot(),
+        Plot(
+            x='profiles_1d/0/grid/rho_tor_norm',
+            y='profiles_1d/0/t_i_average',
+            xlabel='Rho tor.',
+            ylabel='Ion temperature',
+        )
+    ]
