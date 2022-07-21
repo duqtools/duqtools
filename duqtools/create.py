0dcc8f1b31786af64e748a309b4f3a59b00f4b45
Victor Azizi <victor@lipsum.eu>
2022-07-21 15:57:11 +0200

    working create prototype, but it needs to be much cleaner

--- duqtools/create.py
+++ duqtools/create.py
@@ -7,6 +7,7 @@ from .ids import IDSMapping, ImasHandle
 from .ids.dimensions import apply_model
 from .matrix_samplers import get_matrix_sampler
 from .models import WorkDirectory
+from .operations import confirm_operations, op_queue
 from .schema.runs import Runs
 from .system import get_system
 
@@ -28,6 +29,26 @@ def fail_if_locations_exist(locations: Iterable[ImasHandle]):
             'remove or `--force` to override.')
 
 
+def apply_combination(target_in: ImasHandle, combination) -> None:
+    core_profiles = target_in.get('core_profiles')
+    ids_mapping = IDSMapping(core_profiles)
+
+    for model in combination:
+        apply_model(model, ids_mapping)
+
+        logger.info('Writing data entry: %s', target_in)
+        with target_in.open() as data_entry_target:
+            core_profiles.put(db_entry=data_entry_target)
+
+
+def write_runs_file(runs: list, workspace) -> None:
+
+    runs = Runs.parse_obj(runs)
+    with open(workspace.runs_yaml, 'w') as f:
+        runs.yaml(stream=f)
+
+
+@confirm_operations
 def create(*, force, dry_run, **kwargs):
     """Create input for jetto and IDS data structures.
 
@@ -75,8 +96,10 @@ def create(*, force, dry_run, **kwargs):
     for i, combination in enumerate(combinations):
         run_name = f'{RUN_PREFIX}{i:04d}'
         run_drc = workspace.cwd / run_name
-        if not dry_run:
-            run_drc.mkdir(parents=True, exist_ok=force)
+
+        op_queue.add(action=lambda run_drc=run_drc: run_drc.mkdir(
+            parents=True, exist_ok=force),
+                     description=f'Create folder {run_drc}')
 
         target_in = ImasHandle(db=options.data.db,
                                shot=source.shot,
@@ -85,25 +108,28 @@ def create(*, force, dry_run, **kwargs):
                                 shot=source.shot,
                                 run=options.data.run_out_start_at + i)
 
-        source.copy_ids_entry_to(target_in)
+        op_queue.add(action=lambda: source.copy_ids_entry_to(target_in),
+                     description=f'Create {target_in} from {source}')
 
-        core_profiles = target_in.get('core_profiles')
-        ids_mapping = IDSMapping(core_profiles)
+        op_queue.add(
+            action=lambda combination=combination, target_in=target_in,
+            target_out=target_out: apply_combination(target_in, combination),
+            description=f'Applying combination {combination} to {target_in}')
 
-        if not dry_run:
-            for model in combination:
-                apply_model(model, ids_mapping)
+        op_queue.add(
+            action=lambda run_drc=run_drc: system.copy_from_template(
+                template_drc, run_drc),
+            description=f'Copying template from {template_drc} to {run_drc}')
 
-            logger.info('Writing data entry: %s', target_in)
-            with target_in.open() as data_entry_target:
-                core_profiles.put(db_entry=data_entry_target)
+        op_queue.add(action=lambda run_name=run_name: system.write_batchfile(
+            workspace, run_name),
+                     description=f'Writing new batchfile for {run_name}')
 
-        system.copy_from_template(template_drc, run_drc)
-        system.write_batchfile(workspace, run_name)
-
-        system.update_imas_locations(run=run_drc,
-                                     inp=target_in,
-                                     out=target_out)
+        op_queue.add(action=lambda run_drc=run_drc, target_in=target_in,
+                     target_out=target_out: system.update_imas_locations(
+                         run=run_drc, inp=target_in, out=target_out),
+                     description=f'Updating imas locations of {run_drc} to \n'
+                     'in: {target_in}\nout: {target_out}')
 
         runs.append({
             'dirname': run_name,
@@ -112,8 +138,5 @@ def create(*, force, dry_run, **kwargs):
             'operations': combination
         })
 
-    if not dry_run:
-        runs = Runs.parse_obj(runs)
-
-        with open(workspace.runs_yaml, 'w') as f:
-            runs.yaml(stream=f)
+    op_queue.add(action=lambda: write_runs_file(runs, workspace),
+                 description=f'Writing out {workspace.runs_yaml}')
--- duqtools/operations.py
+++ duqtools/operations.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from queue import SimpleQueue
 from typing import Callable
 
@@ -7,8 +8,12 @@ from pydantic import Field
 
 from .schema import BaseModel
 
+logger = logging.getLogger(__name__)
+
 
 class Operation(BaseModel):
+    """Operation, simple class which has a callable action."""
+
     description: str = Field(
         description='description of the operation to be done')
     action: Callable = Field(
@@ -16,6 +21,7 @@ class Operation(BaseModel):
         'decide to apply this operation')
 
     def __call__(self) -> Operation:
+        logger.info(f'-: {self.description}')
         self.action()
         return self
 
@@ -31,6 +37,11 @@ class Operations(SimpleQueue):
         if not Operations._instance:
             Operations._instance = super().__new__(cls)
         return Operations._instance
+
+    def add(self, action: Callable, description: str) -> None:
+        """convenience wrapper around put."""
+
+        self.put(Operation(action=action, description=description))
 
     def put(self, item: Operation) -> None:  # type: ignore
         """Restrict our diet to Operation objects only."""
@@ -49,3 +60,15 @@ class Operations(SimpleQueue):
 
 
 op_queue = Operations()
+
+
+def confirm_operations(func):
+    """Decorator which confirms and applies queued operations after the
+    function."""
+
+    def wrapper(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        op_queue.apply_all()
+        return ret
+
+    return wrapper
