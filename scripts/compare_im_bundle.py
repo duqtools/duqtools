b9a450930b8b27b852bb885d8cb91b35fd25d793
Michele Marin <michele.marin@epfl.ch>
2022-08-11 13:43:32 +0200

    Getting compare_bundle and compare_exp up to speed and providing examples

--- scripts/compare_im_bundle.py
+++ scripts/compare_im_bundle.py
@@ -3,100 +3,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 import compare_im_runs
 import prepare_im_input
+import compare_im_exp
+
+'''
+
+Shows the comparison for multiple signals, multiple plots and multiple settings
+
+Example of usage:
+
+plot_errors('/afs/eufus.eu/user/g/user/mylist.txt', ['summary.global_quantities.v_loop.value', 'core_profiles.profiles_1d[].q', 'summary.global_quantities.li.value', 'te'], plot_type = 1, time_begin = 0.02, time_end = 0.1)
 
 
 '''
-I am fairly sure this is not a nice way to do this, but I'll think of a better way later
-'''
-
-class CompareImRunsArguments:
-    def __init__(
-        self,
-        backend = "mdsplus",
-        database = None,
-        shot = None,
-        run = None,
-        user = None,
-        version = 3,
-        time_begin = None,
-        time_end = None,
-        time_out = None,
-        signal = None,
-        source = ["total"],
-        transport = ['transport_solver'],
-        steady_state = False,
-        show_plot = False,
-        plot_uniform_basis = False,
-        analyze_traces = None,
-        analyze_profiles = None,
-        change_sign = False,
-        multi_var_function = None
-    ):
-
-        self.backend = backend
-        self.database = database
-        self.shot = shot
-        self.run = run
-        self.signal = signal
-        self.version = version
-        self.time_begin = time_begin
-        self.time_end = time_end
-        self.time_out = time_out
-        self.user = user
-        if not user:
-            self.user = [os.getenv("USER")]
-        self.source = source
-        self.transport = transport
-        self.steady_state = steady_state
-        self.show_plot = show_plot
-        self.plot_uniform_basis = plot_uniform_basis
-        self.analyze_traces = analyze_traces
-        self.analyze_profiles = analyze_profiles
-        self.change_sign = change_sign
-        self.multi_var_function = multi_var_function
 
 
-def get_error(shot, run_input, run_output, signal, db = 'tcv'):
+exp_signal_list = ['te', 'ne', 'ti', 'ni']
+
+
+def get_error(shot, run_input, run_output, signal, time_begin = None, time_end = None, db = 'tcv'):
 
     # The time compared is the time of the simulation. Still on the experiment time array
     summary = prepare_im_input.open_and_get_summary(db, shot, run_output)
-    time_begin, time_end = min(summary.time), max(summary.time)
 
-    # Temporary hopefully. Only a few signals need to change sign. Listing them here
-    change_sign = False
-    if signal == 'summary.global_quantities.v_loop.value':
-        summary_input = prepare_im_input.open_and_get_summary(db, shot, run_input)
-        average_input = np.average(summary_input.global_quantities.v_loop.value)
-        average_output = np.average(summary.global_quantities.v_loop.value)
-        if np.sign(average_input) != np.sign(average_output):
-            change_sign = True
+    username = os.getenv("USER")
+    userlist = [username]
+    dblist = [db]
+    shotlist = [shot]
+    runlist = [run_input,run_output]
+    # Exclude very fast equilibrium readjusting on the first few timesteps
+    if not time_begin:
+        time_begin = min(summary.time) + 0.01
+    if not time_end:
+        time_end = max(summary.time)
+    signal = [signal]
+    show_plot = False
 
-    if signal == 'core_profiles.profiles_1d[].q':
-        core_profiles_input = prepare_im_input.open_and_get_core_profiles(db, shot, run_input)
-        core_profiles_output = prepare_im_input.open_and_get_core_profiles(db, shot, run_output)
-        average_input = np.average(core_profiles_input.profiles_1d[0].q)
-        average_output = np.average(core_profiles_output.profiles_1d[0].q)
-        if np.sign(average_input) != np.sign(average_output):
-            change_sign = True
+    time_averages, time_error_averages, profile_error_averages = compare_im_runs.compare_runs(signal, dblist, shotlist, runlist, time_begin, userlist = userlist, time_end=time_end, plot=False, analyze=True, correct_sign=True, signal_operations=None)
 
+    errors = []
+    for signal in time_error_averages:
+        for run_tag in time_error_averages[signal]:
+            errors.append(time_error_averages[signal][run_tag])
 
-    args = CompareImRunsArguments(
-        database = [db],
-        shot = [shot],
-        run = [run_input,run_output],
-        time_begin = time_begin,
-        time_end = time_end,
-        signal = [signal],
-        change_sign = change_sign,
-        show_plot = False
-    )
+    for signal in profile_error_averages:
+        for run_tag in profile_error_averages[signal]:
+            errors.append(profile_error_averages[signal][run_tag])
 
-    error_signal = compare_im_runs.main(args)
-
-    return(error_signal)
+    return errors
 
 
-def plot_errors(filename, signal_list):
+def get_exp_error(shot, run_input, run_output, signal, time_begin = None, time_end = None, db = 'tcv'):
+
+    summary = prepare_im_input.open_and_get_summary(db, shot, run_output)
+    if not time_begin:
+        time_begin = min(summary.time) + 0.01
+    if not time_end:
+        time_end = max(summary.time)
+
+    return(compare_im_exp.plot_exp_vs_model(db, shot, run_input, run_output, time_begin, time_end, signals = [signal], verbose = 0))
+
+
+def plot_errors(filename, signal_list, time_begin = None, time_end = None, plot_type = 1):
 
     file_runs = open(filename, 'r')
     lines = file_runs.readlines()
@@ -128,48 +95,68 @@ def plot_errors(filename, signal_list):
         fig, ax = plt.subplots(2,2)
         num_columns = 2
 
-    print(ax)
-    #exit()
+    if plot_type == 1:
+        for isignal, signal in enumerate(signal_list):
+            # Needed for aoutoformatting
+            icolumns = int(isignal/num_columns)
+            iraws = isignal % num_columns
 
+            labels = shot_list
+            x = np.arange(len(labels))  # the label locations
+            width = 1/(num_run_series+1)  # the width of the bars
 
-    for isignal, signal in enumerate(signal_list):
-        # Needed for aoutoformatting
-        icolumns = int(isignal/num_columns)
-        iraws = isignal % num_columns
+            for run_serie in range(num_run_series):
+                errors = []
+                #if signal not in exp_signal_list:
+                for shot, run_input, run_output in zip(shot_list, run_input_list, run_output_list[:,run_serie]):
+                    if signal not in exp_signal_list:
+                         errors.append(get_error(shot, run_input, run_output, signal, time_begin = time_begin, time_end = time_end)[0])
+                    else:
+                         errors.append(get_exp_error(shot, run_input, run_output, signal, time_begin = time_begin, time_end = time_end)[0])
 
-        print(icolumns, iraws)
+                rects = ax[icolumns][iraws].bar(x - width + width/num_run_series + run_serie*width, errors, width, label=labels_plot[run_serie])
 
-        labels = shot_list
-        x = np.arange(len(labels))  # the label locations
-        width = 1/(num_run_series+1)  # the width of the bars
+            # Add some text for labels, title and custom x-axis tick labels, etc.
+            ax[icolumns][iraws].set_ylabel('Distance')
+            ax[icolumns][iraws].set_title(signal)
+            ax[icolumns][iraws].set_xticks(x, labels)
+            ax[icolumns][iraws].legend()
 
-        for run_serie in range(num_run_series):
-            errors = []
+            fig.tight_layout()
 
-            for shot, run_input, run_output in zip(shot_list, run_input_list, run_output_list[:,run_serie]):
-                errors.append(get_error(shot, run_input, run_output, signal)[0])
+        plt.show()
 
-            rects = ax[icolumns][iraws].bar(x - width + width/num_run_series + run_serie*width, errors, width, label=labels_plot[run_serie])
-        
-            #ax[isignal].bar_label(rects, padding=3)
-            #ax[isignal].bar_label(rects)
+    elif plot_type == 2:
+        for isignal, signal in enumerate(signal_list):
+            # Needed for aoutoformatting
+            icolumns = int(isignal/num_columns)
+            iraws = isignal % num_columns
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        ax[icolumns][iraws].set_ylabel('Errors')
-        ax[icolumns][iraws].set_title(signal)
-        ax[icolumns][iraws].set_xticks(x, labels)
-        ax[icolumns][iraws].legend()
+            x = np.arange(num_run_series)  # the label locations
+            width = 1/(len(shot_list)+1)  # the width of the bars
 
-        fig.tight_layout()
+            for shot in range(len(shot_list)):
+                errors = []
+                for run_output in run_output_list[shot,:]:
+                    if signal not in exp_signal_list:
+                        errors.append(get_error(shot_list[shot], run_input_list[shot], run_output, signal, time_begin = time_begin, time_end = time_end)[0])
+                    else:
+                        errors.append(get_exp_error(shot_list[shot], run_input_list[shot], run_output, signal, time_begin = time_begin, time_end = time_end)[0])
 
-    plt.show()
+                rects = ax[icolumns][iraws].bar(x - width + width/len(shot_list) + shot*width, errors, width, label=shot_list[shot])
+
+            # Add some text for labels, title and custom x-axis tick labels, etc.
+            ax[icolumns][iraws].set_ylabel('Distance')
+            ax[icolumns][iraws].set_title(signal)
+            ax[icolumns][iraws].set_xticks(x, labels_plot)
+            ax[icolumns][iraws].legend()
+
+            fig.tight_layout()
+
+        plt.show()
 
 
 if __name__ == "__main__":
-    #plot_errors('runs_list.txt', ['summary.global_quantities.v_loop.value'])
-    #plot_errors('runs_list.txt', ['summary.global_quantities.v_loop.value', 'core_profiles.profiles_1d[].q'])
-    #plot_errors('runs_list.txt', ['summary.global_quantities.v_loop.value', 'core_profiles.profiles_1d[].q', 'summary.global_quantities.li.value'])
-    plot_errors('runs_list.txt', ['summary.global_quantities.v_loop.value', 'core_profiles.profiles_1d[].q', 'summary.global_quantities.li.value', 'summary.global_quantities.beta_pol.value'])
 
-
+    print('compare sets of runs')
 
