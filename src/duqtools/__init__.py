f2429fabf75bd877522d2e3d07bd977cceca458e
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-08-25 15:11:39 +0200

    Filter pydantic-yaml explicit root warning (#273)
    * Filter yaml explicit root warning
    * Add comment

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -3,5 +3,11 @@ __email__ = 's.smeets@esciencecenter.nl'
 __version__ = '0.3.0'
 
 import logging
+import warnings
 
 logging.basicConfig(level=logging.INFO)
+
+# https://github.com/CarbonCollective/fusion-dUQtools/issues/264
+warnings.filterwarnings(
+    'ignore',
+    'Explicit custom root behavior not yet implemented for pydantic_yaml')
