15a75a576de42499d30ceebe92237ab3ebb000e2
Victor Azizi <victor@lipsum.eu>
2022-09-15 15:46:32 +0200

    removed code duplication

--- tests/ids/test_rebase.py
+++ tests/ids/test_rebase.py
@@ -78,37 +78,10 @@ def expected_standardized():
 
 
 @pytest.fixture
-def expected_standardized_time():
-    return xr.Dataset.from_dict({
-        'coords': {
-            'time': {
-                'dims': ('time', ),
-                'attrs': {},
-                'data': [1, 2, 3]
-            },
-            'xvar': {
-                'dims': ('xvar', ),
-                'attrs': {},
-                'data': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            }
-        },
-        'attrs': {},
-        'dims': {
-            'time': 3,
-            'xvar': 10
-        },
-        'data_vars': {
-            'yvar': {
-                'dims': ('time', 'xvar'),
-                'attrs': {},
-                'data': [
-                    [0.0, 1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0, 81.0],
-                    [0.0, 2.0, 4.0, 10.0, 16.0, 26.0, 36.0, 50.0, 64.0, 82.0],
-                    [0.0, 3.0, 6.0, 9.0, 18.0, 27.0, 36.0, 51.0, 66.0, 81.0],
-                ]
-            }
-        }
-    })
+def expected_standardized_time(expected_standardized):
+    expected = expected_standardized.copy()
+    expected['time'] = [1, 2, 3]
+    return expected
 
 
 @pytest.fixture
