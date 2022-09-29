864e434ef91c6b371012905d430caa0ea8c09868
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-09-29 12:07:55 +0200

    Require jetto_tools version (#324)
    * Require jetto_tools version
    * Set main requires

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -1,3 +1,9 @@
+# https://setuptools.pypa.io/en/latest/pkg_resources.html#workingset-objects
+import __main__
+
+__main__.__requires__ = ['jetto_tools>=1.8.6']
+import pkg_resources
+
 __author__ = 'Carbon Collective'
 __email__ = 's.smeets@esciencecenter.nl'
 __version__ = '1.0.0'
