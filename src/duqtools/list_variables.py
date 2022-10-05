74fea447c7084d2af266c018b3c9a19abb187f04
Stef Smeets <s.smeets@esciencecenter.nl>
2022-10-05 09:05:24 +0200

    Update function to list variables

--- src/duqtools/list_variables.py
+++ src/duqtools/list_variables.py
@@ -3,20 +3,31 @@ from pydantic import ValidationError
 from duqtools.config import cfg, var_lookup
 
 
-def list_variables(config, **kwargs):
-    print('variables.yaml')
-    for name, var in var_lookup.items():
-        print('    ', var.name, var.type)
+def list_variables(*, config, **kwargs):
+    """List variables in `variables.yaml` and config/`duqtools.yaml` if
+    present.
 
-    print()
-
+    Parameters
+    ----------
+    config : str
+        Name of the config file to use
+    **kwargs
+        Unused.
+    """
     try:
         cfg.parse_file(config)
     except FileNotFoundError:
         print(f'Could not find: {config}')
     except ValidationError as e:
         exit(e)
-    else:
-        print(config)
-        for var in config.extra_variables:
-            print('    ', var)
+        print(f'*: defined by {config}')
+    finally:
+        extra_variables = cfg.extra_variables.to_variable_dict(
+        ) if cfg.extra_variables else {}
+
+    print('variables.yaml')
+    for name, var in var_lookup.items():
+        star = '*' if name in extra_variables else ''
+        print('    ', var.name, star)
+
+    print()
