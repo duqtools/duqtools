b317aa2bf6082c455411778ac2e33fdb93d6622f
Stef Smeets <s.smeets@esciencecenter.nl>
2022-10-27 16:40:18 +0200

    Add matplotlib scripts

--- scripts/plot_with_matplotlib.py
+++ scripts/plot_with_matplotlib.py
@@ -1,10 +1,15 @@
+import matplotlib.pyplot as plt
+
 from duqtools.api import ImasHandle
 
 handle = ImasHandle.from_string('g2vazizi/jet/94875/8000')
 
 x_var = 'rho_tor_norm'
 y_var = 'zeff'
+time_var = 'time'
 
-dataset = handle.get_variables(variables=(x_var, y_var))
+dataset = handle.get_variables(variables=(x_var, y_var, time_var))
 
-# MPL stuff
+fig = dataset.plot.scatter(x_var, y_var, hue='time', marker='.')
+
+plt.show()
--- scripts/plot_with_matplotlib_multi.py
+++ scripts/plot_with_matplotlib_multi.py
@@ -1,21 +1,27 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import xarray as xr
 
-from duqtools.api import ImasHandle
+from duqtools.api import ImasHandle, standardize_datasets
 
 runs = 8000, 8001, 8002
 
 x_var = 'rho_tor_norm'
 y_var = 't_i_ave'
+time_var = 'time'
 
 ds_list = []
 
 for run in runs:
     handle = ImasHandle(user='g2vazizi', db='jet', shot='94875', run=run)
 
-    ds = handle.get_variables(variables=(x_var, y_var))
+    ds = handle.get_variables(variables=(x_var, y_var, time_var))
     ds_list.append(ds)
+
+ds_list = standardize_datasets(ds_list, grid_var=x_var, time_var=time_var)
 
 dataset = xr.concat(ds_list, pd.Index(runs, name='run'))
 
-# MPL stuff
+fig = dataset.plot.scatter(x_var, y_var, hue='time', col='run', marker='.')
+
+plt.show()
