118494d76bebd8361407d76abff6793ba12f9280
Victor Azizi <victor@lipsum.eu>
2022-11-24 10:37:55 +0100

    more verbosity towards user

--- src/duqtools/merge.py
+++ src/duqtools/merge.py
@@ -4,6 +4,7 @@ import logging
 
 from .config import cfg, var_lookup
 from .ids import ImasHandle, merge_data
+from .operations import op_queue
 from .utils import partition, read_imas_handles_from_file
 
 logger = logging.getLogger(__name__)
@@ -17,6 +18,14 @@ def merge(**kwargs):
 
     handles = read_imas_handles_from_file(cfg.merge.data).values()
 
+    for handle in handles:
+        op_queue.add(action=lambda: None,
+                     description='Merging source',
+                     extra_description=f'{handle}')
+    op_queue.add(action=lambda: None,
+                 description='Merging template',
+                 extra_description=f'{template}')
+
     debug('Merge input: %s', template)
     debug('Merge output: %s', target)
 
@@ -26,7 +35,13 @@ def merge(**kwargs):
         _, variables = partition(lambda var: var.type == 'IDS-variable',
                                  var_lookup.values())
         variables = list(variables)
+        op_queue.add(action=lambda: None,
+                     description='Merging all known variables')
     else:
         variables = [var_lookup[name] for name in cfg.merge.variables]
+        for variable in variables:
+            op_queue.add(action=lambda: None,
+                         description='Merging variable',
+                         extra_description=f'{variable.name}')
 
     merge_data(handles, target, variables)
