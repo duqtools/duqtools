71e063aecfa55611a2880aa8921295d3f90390bd
Victor Azizi <victor@lipsum.eu>
2023-01-23 14:50:55 +0100

    fixed test

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -144,7 +144,7 @@ def test_example_plot(cmdline_workdir):
     if imas_mocked:
         pytest.xfail('Imas needed for plotting Imas data')
 
-    cmd = ('duqtools plot -h public/test/11111/6666 -v t_i_ave').split()
+    cmd = ('duqtools plot -h public/jet/90350/2 -v t_i_ave').split()
 
     with work_directory(cmdline_workdir):
         result = sp.run(cmd)
