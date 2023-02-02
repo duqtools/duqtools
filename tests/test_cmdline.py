52a03cc749102bb9381e569c2d2c09ffc2ad9dd7
Victor Azizi <victor@lipsum.eu>
2023-02-02 11:58:38 +0100

    updated imas locations in test

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -66,11 +66,11 @@ def test_example_create(cmdline_workdir, system):
         for i in range(3):
             if system == 'jetto-v210921':
                 p = Path(
-                    f'/opt/imas/shared/imasdb/test/3/0/ids_11111700{i}.datafile'
+                    f'/opt/imas/shared/imasdb/test/3/0/ids_90350000{i}.datafile'
                 )
             else:
                 p = Path(cmdline_workdir,
-                         f'run_000{i}/imasdb/test/3/0/ids_111110001.datafile')
+                         f'run_000{i}/imasdb/test/3/0/ids_903500001.datafile')
             assert p.exists()
 
 
@@ -79,10 +79,10 @@ def test_example_recreate(cmdline_workdir, system):
     cmd = 'duqtools recreate run_0000 -c config.yaml --yes'.split()
 
     if system == 'jetto-v210921':
-        p = Path('/opt/imas/shared/imasdb/test/3/0/ids_111117000.datafile')
+        p = Path('/opt/imas/shared/imasdb/test/3/0/ids_903507000.datafile')
     else:
         p = Path(cmdline_workdir,
-                 'run_0000/imasdb/test/3/0/ids_111110001.datafile')
+                 'run_0000/imasdb/test/3/0/ids_903500001.datafile')
 
     p.unlink()
     assert not p.exists()
