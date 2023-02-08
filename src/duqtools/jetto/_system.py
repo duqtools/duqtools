ddf1f709405868ed62ef0d263afe44524aef2d7a
Stef Smeets <stefsmeets@users.noreply.github.com>
2023-02-08 13:45:59 +0100

    Raise if restart files exist in template (#499)
    * Raise if restart files exist

--- src/duqtools/jetto/_system.py
+++ src/duqtools/jetto/_system.py
@@ -195,6 +195,12 @@ class BaseJettoSystem(AbstractSystem):
         for regex in _EXTRA_FILE_REGEXES:
             jetto_extra.extend(filter(regex.match, all_files))
 
+        if 'jetto.restart' in jetto_extra or 'jetto.srestart' in jetto_extra:
+            raise OSError(
+                'Template contains `jetto.restart` and/or `jetto.srestart` files. '
+                'Please remove these before running duqtools again. '
+                'More info: https://github.com/duqtools/duqtools/issues/498')
+
         jetto_extra = [str(source_drc / file) for file in jetto_extra]
 
         jetto_template = template.Template(jset=jetto_jset,
