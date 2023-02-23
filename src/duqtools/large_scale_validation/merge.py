8e953e7ee91475788c00bdb7167acf057cb6f50a
Stef Smeets <s.smeets@esciencecenter.nl>
2023-02-23 11:49:47 +0100

    Implement first version of duqduq merge

--- src/duqtools/large_scale_validation/merge.py
+++ src/duqtools/large_scale_validation/merge.py
@@ -1,15 +1,39 @@
+import logging
 from pathlib import Path
 
-from ..config import cfg
-from ..merge import merge as duqtools_merge
+from duqtools.api import ImasHandle
+
+from ..config import cfg, var_lookup
+from ..merge import _merge
+from ..operations import op_queue
+from ..utils import read_imas_handles_from_file
+
+logger = logging.getLogger(__name__)
 
 
 def merge(**kwargs):
     cwd = Path.cwd()
+
+    variables = tuple(var_lookup.filter_type('IDS-variable').values())
+    op_queue.info(description='Merging all known variables')
 
     config_files = cwd.glob('**/duqtools.yaml')
 
     for config_file in config_files:
         cfg.parse_file(config_file)
 
-        duqtools_merge()
+        data_csv = config_file.parent / 'data.csv'
+        handles = read_imas_handles_from_file(data_csv)
+
+        template_data = ImasHandle.parse_obj(cfg.create.template_data)
+
+        target_data = template_data.copy()
+        target_data.user = str(cfg.create.runs_dir / 'imasdb')
+
+        _merge(
+            variables=variables,
+            handles=handles,
+            target=target_data,
+            template=template_data,
+            force=False,
+        )
--- src/duqtools/merge.py
+++ src/duqtools/merge.py
@@ -1,14 +1,55 @@
 from __future__ import annotations
 
 import logging
+from typing import Sequence
 
 from .config import var_lookup
 from .ids import ImasHandle, merge_data
 from .operations import op_queue
+from .schema import IDSVariableModel
 from .utils import read_imas_handles_from_file
 
 logger = logging.getLogger(__name__)
 info, debug = logger.info, logger.debug
+
+
+def _merge(*,
+           handles: Sequence[ImasHandle],
+           template: ImasHandle,
+           target: ImasHandle,
+           variables: Sequence[IDSVariableModel],
+           force: bool = False):
+    """Merge mas data.
+
+    Parameters
+    ----------
+    handles : Sequence[ImasHandle]
+        These are the imas handles that will be merged.
+    template : ImasHandle
+        This handle is the template that is used for the new data.
+    target : ImasHandle
+        This handle is the target location where the merged data will be stored.
+    variables : Sequence[Variable
+        These are the IDS variables to be merged.
+    force : bool
+        Force overwriting existing files.
+    """
+    for handle in handles:
+        op_queue.info(description='Source for merge',
+                      extra_description=f'{handle}')
+
+    op_queue.add(description='Template for merge',
+                 extra_description=f'{template}')
+
+    if target.exists() and not force:
+        op_queue.add_no_op(description='Abort merge',
+                           extra_description=f'{target} already exists, '
+                           'use --force to overwrite')
+        return
+
+    template.copy_data_to(target)
+
+    merge_data(handles, target, variables)
 
 
 def merge(*, merge_all: bool, target: str, template: str, handles: list[str],
@@ -22,11 +63,7 @@ def merge(*, merge_all: bool, target: str, template: str, handles: list[str],
     for file in input_files:
         handles = handles + list(read_imas_handles_from_file(file).values())
 
-    handles = set(handles)  # Remove duplicate handles
-
-    for handle in handles:
-        op_queue.info(description='Source for merge',
-                      extra_description=f'{handle}')
+    handles = list(set(handles))  # Remove duplicate handles
 
     if merge_all:
         ids_variables = tuple(var_lookup.filter_type('IDS-variable').values())
@@ -42,15 +79,9 @@ def merge(*, merge_all: bool, target: str, template: str, handles: list[str],
             op_queue.info(description='Variable for merge',
                           extra_description=f'{variable.name}')
 
-    op_queue.add(description='Template for merge',
-                 extra_description=f'{template}')
-
-    if target.exists() and not force:
-        op_queue.add_no_op(description='Abort merge',
-                           extra_description=f'{target} already exists, '
-                           'use --force to overwrite')
-        return
-
-    template.copy_data_to(target)
-
-    merge_data(handles, target, ids_variables)
+    _merge(
+        handles=handles,
+        template=template,
+        target=target,
+        variables=ids_variables,
+    )
