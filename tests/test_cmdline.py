56a7c525d2567bf1f7a335e4000c83908e24dd50
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-02 11:16:53 +0100

    Stick with jet

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -51,7 +51,7 @@ def cmdline_workdir(tmp_path_factory, system):
     if system == 'v210921':
         for i in range(2):
             p = Path(cmdline_workdir,
-                     f'run_000{i}/imasdb/test/3/0/ids_111110001.datafile')
+                     f'run_000{i}/imasdb/jet/3/0/ids_1230001.datafile')
             p.unlink()
 
 
@@ -66,11 +66,11 @@ def test_example_create(cmdline_workdir, system):
         for i in range(2):
             if system == 'jetto-v210921':
                 p = Path(
-                    f'/opt/imas/shared/imasdb/test/3/0/ids_11111700{i}.datafile'
+                    f'/opt/imas/shared/imasdb/jet/3/0/ids_123070{i}.datafile'
                 )
             else:
                 p = Path(cmdline_workdir,
-                         f'run_000{i}/imasdb/test/3/0/ids_111110001.datafile')
+                         f'run_000{i}/imasdb/jet/3/0/ids_1230001.datafile')
             assert p.exists()
 
 
@@ -79,10 +79,10 @@ def test_example_recreate(cmdline_workdir, system):
     cmd = 'duqtools recreate run_0000 -c config.yaml --yes'.split()
 
     if system == 'jetto-v210921':
-        p = Path('/opt/imas/shared/imasdb/test/3/0/ids_111117000.datafile')
+        p = Path('/opt/imas/shared/imasdb/jet/3/0/ids_1230700.datafile')
     else:
         p = Path(cmdline_workdir,
-                 'run_0000/imasdb/test/3/0/ids_111110001.datafile')
+                 'run_0000/imasdb/jet/3/0/ids_1230001.datafile')
 
     p.unlink()
     assert not p.exists()
@@ -144,7 +144,7 @@ def test_example_plot(cmdline_workdir):
     if imas_mocked:
         pytest.xfail('Imas needed for plotting Imas data')
 
-    cmd = ('duqtools plot -h public/test/11111/6666 -v zeff').split()
+    cmd = ('duqtools plot -h public/jet/123/1 -v zeff').split()
 
     with work_directory(cmdline_workdir):
         result = sp.run(cmd)
