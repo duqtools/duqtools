83a9a9cc4c2ec57e84cdd3552d94b2502d65d106
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-02 14:43:21 +0100

    Update syntax

--- src/duqtools/data/dash/_shared.py
+++ src/duqtools/data/dash/_shared.py
@@ -2,11 +2,16 @@ import sys
 from pathlib import Path
 from typing import Sequence
 
-import streamlit as st
 import xarray as xr
 
 from duqtools.api import ImasHandle, standardize_grid_and_time
 from duqtools.config import var_lookup
+
+try:
+    from streamlit import cache_data
+except ImportError:
+    # work-around for streamlit<1.18
+    from streamlit import experimental_memo as cache_data
 
 try:
     default_workdir = sys.argv[1]
@@ -17,17 +22,17 @@ PREFIX = 'profiles_1d/*'
 PREFIX0 = 'profiles_1d/0'
 
 
-@st.experimental_memo
+@cache_data
 def get_ids_options():
     return tuple(var_lookup.filter_type('IDS-variable').keys())
 
 
-@st.experimental_memo
+@cache_data
 def get_var_options(ids):
     return list(var_lookup.filter_ids(ids).values())
 
 
-@st.experimental_memo
+@cache_data
 def get_variables(*, ids: str, x_key: str, y_keys: Sequence[str]):
     return {
         'time_var': 'time',
@@ -36,7 +41,7 @@ def get_variables(*, ids: str, x_key: str, y_keys: Sequence[str]):
     }
 
 
-@st.experimental_memo
+@cache_data
 def _get_dataset(handles, variable):
     data_var = variable['name']
     time_var = variable['dims'][0]
