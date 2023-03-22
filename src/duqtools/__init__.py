9f1402e7c3522d5fa06c2761f08947642de2b42b
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-22 12:16:30 +0100

    Bump version: 1.6.1 → 1.6.2

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -16,7 +16,7 @@ fix_dependencies()
 
 __author__ = 'Stef Smeets'
 __email__ = 's.smeets@esciencecenter.nl'
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 
 import logging  # noqa
 import warnings  # noqa
