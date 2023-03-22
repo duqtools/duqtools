1f94ebbfc8b69edd9be340c405d01a3a99c2a0d9
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-22 12:15:50 +0100

    Fix crash

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -5,7 +5,7 @@ def fix_dependencies():
         'jetto_tools>=1.8.8',
         'pydantic!=1.10.3',
         'pydantic!=1.10.4',
-        'pydantic!=1.10.5'
+        'pydantic!=1.10.5',
         'scipy>=1.09',
         'jinja2>=3.0.0',
     ]
