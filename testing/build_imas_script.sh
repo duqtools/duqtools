3d931952f0e8313c433d7e769838cc8a97de4cbd
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-29 10:31:36 +0200

    Add action for building imas on the CI

---
+++ testing/build_imas_script.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Build a copy of IMAS components need by IMASPy from the access-layer monorepo and
+# data-dictionary repo. You can specify the AL version with $1 (usually "develop" or "master")
+# The DDs will be scanned by IMASPy anyway, but we'll build the default branch as well
+
+# # Script boilerplate
+# [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_dir=$(dirname ${BASH_SOURCE[0]}) || my_dir=$(dirname $0)  # Determine script dir even when sourcing
+
+# common_dir=$my_dir
+# . $common_dir/00_common_bash.sh
+
+# old_bash_state="$(get_bash_state "$old_bash_state")"
+# set -xeuf -o pipefail # Set default script debugging flags
+
+###############
+# Script body #
+###############
+
+export CLASSPATH=`pwd`/saxon9he.jar
+
+cd data-dictionary
+# use the latest tagged version
+export IMAS_VERSION=`git tag | sort -V | tail -n 1`
+git checkout "$IMAS_VERSION"
+make
+
+cd ..
+# INSTALL the access layer locally (this might take a while)
+cd access-layer
+git checkout "$1"
+if [ ! -L xml ]; then
+    ln -s ../data-dictionary/ xml
+fi
+
+export UAL_VERSION=`git tag | sort -V | tail -n 1`
+export IMAS_UDA=no \
+    IMAS_MDSPLUS=no \
+    IMAS_HDF5=no \
+    IMAS_MATLAB=no \
+    IMAS_MEX=no \
+    IMAS_JAVA=no
+
+export IMAS_PREFIX=`pwd`
+export LIBRARY_PATH=`pwd`/lowlevel:${LIBRARY_PATH:=}
+export C_INCLUDE_PATH=`pwd`/lowlevel:${C_INCLUDE_PATH:=}
+export LD_LIBRARY_PATH=`pwd`/lowlevel:`pwd`/cppinterface/lib:${LD_LIBRARY_PATH:=}
+
+cd lowlevel
+make -j`nproc`
+cd ..
+
+pip install numpy cython
+
+cd pythoninterface
+make -j`nproc`
+sed -e 's/imas_[^/."]*/imas/g' -i package/setup.py
+
+# We do not always start from a cleaned virtual environment. Uninstall possible leftover packages
+pip uninstall --yes imas || true
+
+# Install locally
+pip install -e package
+
+# test if we can import the imas module
+python -c 'import imas'
+
+cd ../../
+
+# create a shell script to start python with the right environment variables
+echo IMAS_VERSION=$IMAS_VERSION\
+    UAL_VERSION=$UAL_VERSION\
+    IMAS_PREFIX=$IMAS_PREFIX\
+    LIBRARY_PATH=$LIBRARY_PATH\
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH\
+    python > python.sh
+chmod +x python.sh
