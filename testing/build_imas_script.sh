dfd5ab2d78eb31054d79f85c78fa3512ad59552c
Stef Smeets <s.smeets@esciencecenter.nl>
2023-03-29 14:35:18 +0200

    Clean up

--- testing/build_imas_script.sh
+++ testing/build_imas_script.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Build a copy of IMAS components need by IMASPy from the access-layer monorepo and
-# data-dictionary repo. You can specify the AL version with $1 (usually "develop" or "master")
-# The DDs will be scanned by IMASPy anyway, but we'll build the default branch as well
-
-# # Script boilerplate
-# [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_dir=$(dirname ${BASH_SOURCE[0]}) || my_dir=$(dirname $0)  # Determine script dir even when sourcing
-
-# common_dir=$my_dir
-# . $common_dir/00_common_bash.sh
-
-# old_bash_state="$(get_bash_state "$old_bash_state")"
-# set -xeuf -o pipefail # Set default script debugging flags
-
-###############
-# Script body #
-###############
+# Derived from
+# https://git.iter.org/projects/IMAS/repos/imaspy/browse/envs/common/25_build_imas_git.sh
 
 export CLASSPATH=`pwd`/saxon9he.jar
 
@@ -26,7 +12,6 @@ git checkout "$IMAS_VERSION"
 make
 
 cd ..
-# INSTALL the access layer locally (this might take a while)
 cd access-layer
 if [ ! -L xml ]; then
     ln -s ../data-dictionary/ xml
@@ -55,13 +40,12 @@ cd pythoninterface
 make -j`nproc`
 sed -e 's/imas_[^/."]*/imas/g' -i package/setup.py
 
-# We do not always start from a cleaned virtual environment. Uninstall possible leftover packages
+# We do not always start from a cleaned virtual environment.
+# Uninstall possible leftover packages
 pip uninstall --yes imas || true
 
-# Install locally
 pip install -e package
 
-# test if we can import the imas module
 python -c 'import imas'
 
 cd ../../
@@ -70,3 +54,5 @@ echo "export UAL_VERSION=$UAL_VERSION" >> source_me.sh
 echo "export IMAS_PREFIX=$IMAS_PREFIX" >> source_me.sh
 echo "export LIBRARY_PATH=$LIBRARY_PATH" >> source_me.sh
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> source_me.sh
+
+# Usage: `. source_me.sh`
