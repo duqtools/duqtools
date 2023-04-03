f4c961e5552e2c4aea02891c834dd7bddd160895
Stef Smeets <s.smeets@esciencecenter.nl>
2023-04-03 18:07:37 +0200

    Fix file load
    This somehow works. The machine name must be the path to the
    ids file? User does nothing apparently.

--- testing/open_data.py
+++ testing/open_data.py
@@ -5,8 +5,8 @@ from imas import imasdef
 
 os.environ['IMAS_VERSION'] = '3.38.0'
 
-entry = imas.DBEntry(imasdef.ASCII_BACKEND, 'jet', 123, 1,
-                     '/home/stef/python/duqtools/testing/imasdb/')
+entry = imas.DBEntry(imasdef.ASCII_BACKEND,
+                     '/home/stef/python/duqtools/testing/imasdb/jet', 123, 1)
 
 ret = entry.open()
 print(ret)
