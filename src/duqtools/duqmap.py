17d15c20bf350aea218bc5ddf5713c827cf08c7f
Victor Azizi <victor@lipsum.eu>
2023-04-11 10:55:35 +0200

    added docstring

--- src/duqtools/duqmap.py
+++ src/duqtools/duqmap.py
@@ -32,6 +32,7 @@ def duqmap_run(function: Callable[[Run], Any], **kwargs) -> List[Any]:
 def duqmap_imas(function: Callable[[ImasHandle], Any], **kwargs) -> List[Any]:
     def to_imas_handle(run):
         return run.to_imas_handle()
+
     return _duqmap(function, to_imas_handle, **kwargs)
 
 
@@ -39,6 +40,30 @@ def duqmap(function: Callable[[Run | ImasHandle], Any],
            *,
            runs: Optional[List[Run | Path]] = None,
            **kwargs) -> List[Any]:
+    """duqmap is a mapping function which can be used to map a user defined
+    function `function` over either the runs created by duqtools, or the runs
+    specified by the user in `runs`.
+
+    An important gotcha is that when `Paths` are used to define the runs, duqtools
+    does not know how to associate the corresponding ImasHandles, as that information
+    is not available.  So when using it in this way, it is not possible to provide a
+    function which takes an `ImasHandle` as input.
+
+    Parameters
+    ----------
+    function : Callable[[Run | ImasHandle], Any]
+        function which is called for each run, specified either by `runs`, or implicitly
+        by any available `runs.yaml`
+    runs : Optional[List[Run | Path]]
+        A list of runs over which to operate the function
+    kwargs :
+        optional arguments that need to be passed to each `function` that you provide
+
+    Returns
+    -------
+    List[Any]:
+        A list of anything that your function returns
+    """
     try:
         # Gets the type of the first argument to the function, if it exists
         argument = next(iter(signature(function).parameters.items()))[1]
