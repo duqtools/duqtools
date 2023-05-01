0b2158bf3dbc06bd9c249369824f786b8baaaf1e
Stef Smeets <s.smeets@esciencecenter.nl>
2023-05-01 16:27:07 +0200

    Add method to load config from dict

--- src/duqtools/config/_config.py
+++ src/duqtools/config/_config.py
@@ -19,23 +19,35 @@ from ..schema.cli import ConfigModel
 class Config(ConfigModel):
     ...
 
+    @staticmethod
+    def _update_global_config(new_cfg: Config):
+        from ._variables import var_lookup
+
+        if new_cfg.extra_variables:
+            var_lookup.update(new_cfg.extra_variables.to_variable_dict())
+
+        cfg.__dict__.update(new_cfg.__dict__)
+
+    @classmethod
+    def from_dict(cls, mapping: dict):
+        new_cfg = cls.parse_obj(mapping)
+        cls._update_global_config(new_cfg)
+        return new_cfg
+
+    @classmethod
+    def from_file(cls, path: Union[str, Path]):
+        new_cfg = cls.parse_file(path)
+
+        cls._update_global_config(new_cfg)
+
+        for obj in (cfg, new_cfg):
+            obj._path = path
+
+        return new_cfg
+
 
 def load_config(path: Union[str, Path]) -> Config:
-    global cfg
-
-    new_cfg = Config.parse_file(path)
-
-    from ._variables import var_lookup
-
-    if new_cfg.extra_variables:
-        var_lookup.update(new_cfg.extra_variables.to_variable_dict())
-
-    cfg.__dict__.update(new_cfg.__dict__)
-
-    for obj in (cfg, new_cfg):
-        obj._path = path
-
-    return new_cfg
+    return Config.from_file(path)
 
 
 cfg = Config.construct()
