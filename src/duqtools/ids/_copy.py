88e4ab3d0b1a4b520499fa62aafa4096f391c0d5
Victor Azizi <victor@lipsum.eu>
2023-05-17 15:21:43 +0200

    Re-added old way of copying and DUQTOOLS_LEGACY_IDS_COPY switch

--- src/duqtools/ids/_copy.py
+++ src/duqtools/ids/_copy.py
@@ -1,26 +1,32 @@
 from __future__ import annotations
 
+import logging
 import os
 import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from packaging import version
+
+from .._logging_utils import LoggingContext
 from ..operations import add_to_op_queue
-from ._imas import imas
+from ._imas import Parser, imas
 
 if TYPE_CHECKING:
     from .ids import ImasHandle
 
 
-def get_imas_version():
+def get_imas_ual_version():
     """Get imas/ual versions.
 
-    Changed in 3.10.6, find new ways to find this info
+    Parsed from a string like: `imas_3_34_0_ual_4_9_3`
     """
+    vsplit = imas.names[0].split('_')
 
-    imas_version = imas._ual_lowlevel.sys.version_info
+    imas_version = version.parse('.'.join(vsplit[1:4]))
+    ual_version = version.parse('.'.join(vsplit[5:8]))
 
-    return imas_version
+    return imas_version, ual_version
 
 
 def add_provenance_info(handle: ImasHandle, ids: str = 'core_profiles'):
@@ -64,8 +70,8 @@ def add_provenance_info(handle: ImasHandle, ids: str = 'core_profiles'):
         entry.put(db_entry=data_entry_target)
 
 
-@add_to_op_queue('Copy ids from template to', '{target}', quiet=True)
-def copy_ids_entry(source: ImasHandle, target: ImasHandle):
+def copy_ids_entry_complex(source: ImasHandle, target: ImasHandle):
+    """Old way of copying by reading and writing via IMAS."""
     """Copies the ids entry to a new location.
 
     Parameters
@@ -82,9 +88,65 @@ def copy_ids_entry(source: ImasHandle, target: ImasHandle):
     """
     target.validate()
 
-    imas_version = get_imas_version()
-    os.environ[
-        'IMAS_VERSION'] = f'{imas_version.major}.{imas_version.minor},{imas_version.micro}'
+    imas_version, _ = get_imas_ual_version()
+
+    idss_in = imas.ids(source.shot, source.run)
+    op = idss_in.open_env(source.user, source.db, str(imas_version.major))
+
+    ids_not_found = op[0] < 0
+    if ids_not_found:
+        raise KeyError('The entry you are trying to copy does not exist')
+
+    idss_out = imas.ids(target.shot, target.run)
+
+    idss_out.create_env(target.user, target.db, str(imas_version.major))
+    idx = idss_out.expIdx
+
+    parser = Parser.load_idsdef()
+
+    # Temporarily hide warnings, because this loop is very spammy
+    with LoggingContext(level=logging.CRITICAL):
+
+        for ids_info in parser.idss:
+            name = ids_info['name']
+            maxoccur = int(ids_info['maxoccur'])
+
+            if name in ('ec_launchers', 'numerics', 'sdn'):
+                continue
+
+            for i in range(maxoccur + 1):
+                ids = idss_in.__dict__[name]
+                ids.get(i)
+                ids.setExpIdx(idx)  # this line sets the index to the output
+                ids.put(i)
+
+    idss_in.close()
+    idss_out.close()
+
+    add_provenance_info(handle=target)
+
+
+@add_to_op_queue('Copy ids from template to', '{target}', quiet=True)
+def copy_ids_entry(source: ImasHandle, target: ImasHandle):
+    """Copies the ids entry to a new location.
+
+    Parameters
+    ----------
+    source : ImasHandle
+        Source ids entry
+    target : ImasHandle
+        Target ids entry
+
+    Raises
+    ------
+    KeyError
+        If the IDS entry you are trying to copy does not exist.
+    """
+
+    if os.environ.get('DUQTOOLS_LEGACY_IDS_COPY'):
+        return copy_ids_entry_complex(source, target)
+
+    target.validate()
 
     for src_file, dst_file in zip(source.paths(), target.paths()):
         shutil.copyfile(src_file, dst_file)
