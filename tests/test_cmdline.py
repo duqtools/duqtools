226e4223d78b779d5acbea723e4da1832742f991
Stef Smeets <s.smeets@esciencecenter.nl>
2023-10-12 16:57:51 +0200

    Add tests for status, setup and submit

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -7,8 +7,11 @@ from pathlib import Path
 import pytest
 import yaml
 from click.testing import CliRunner
+from jetto_tools.template import TemplateError
+from pytest import TEST_DATA
 
 from duqtools import cli
+from duqtools.config import load_config
 from duqtools.utils import work_directory
 
 imas = pytest.importorskip('imas',
@@ -73,6 +76,44 @@ def test_create(tmpworkdir):
 
 
 @pytest.mark.dependency()
+def test_submit(tmpworkdir):
+    with work_directory(tmpworkdir):
+        runner = CliRunner()
+        ret = runner.invoke(cli.cli_submit)
+
+    assert ret.exit_code == 0
+
+
+@pytest.mark.dependency()
+def test_status(tmpworkdir):
+    with work_directory(tmpworkdir):
+        runner = CliRunner()
+        ret = runner.invoke(cli.cli_status)
+
+    assert ret.exit_code == 0
+
+
+@pytest.mark.dependency()
+def test_status_progress(tmpworkdir):
+    with work_directory(tmpworkdir):
+        runner = CliRunner()
+        ret = runner.invoke(cli.cli_status, ['--progress'])
+
+    assert ret.exit_code == 0
+
+
+@pytest.mark.dependency()
+def test_status_detailed(tmpworkdir):
+
+    with work_directory(tmpworkdir):
+        runner = CliRunner()
+        ret = runner.invoke(cli.cli_status, ['--detailed'])
+
+    assert ret.exit_code == 1
+    assert isinstance(ret.exc_info[1], TemplateError)
+
+
+@pytest.mark.dependency()
 def test_merge(tmpworkdir):
     tmpworkdir = Path(tmpworkdir).absolute()
     imasdbdir = Path(tmpworkdir, 'run_0000', 'imasdb')
@@ -92,3 +133,29 @@ def test_merge(tmpworkdir):
 
     for fn in 'ids_1231001.characteristics', 'ids_1231001.datafile', 'ids_1231001.tree':
         assert Path(imasdbdir, 'jet', '3', '0', fn).exists()
+
+
+def test_setup(tmpdir):
+    # must run in a different tmpdir becuase it generates duqtools.yaml
+    template = TEST_DATA / 'config_jetto.yaml.template'
+
+    with work_directory(tmpdir):
+        runner = CliRunner()
+        ret = runner.invoke(cli.cli_setup, [
+            '--force',
+            '--yes',
+            *('--template', str(template)),
+            *('--run_name', 'testrun'),
+            *('--handle', f'{IMASDB}/jet/123/1'),
+        ])
+
+    assert ret.exit_code == 0
+
+    config = tmpdir / 'testrun' / 'duqtools.yaml'
+    assert config.exists()
+
+    cfg = load_config(config)
+    assert cfg.create.operations[0].value == 2.96
+    assert cfg.create.operations[1].value == 2.1155
+    assert cfg.create.operations[2].value == 45.75
+    assert cfg.create.operations[3].value == 45.76
