f87356c844e49c51279d900ef8d4c506dc3e66c7
Stef Smeets <s.smeets@esciencecenter.nl>
2023-11-09 16:55:36 +0100

    Fix version on gateway

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -8,6 +8,7 @@ def fix_dependencies():
         'jetto_tools>=1.8.8',
         'scipy>=1.09',
         'jinja2>=3.0.0',
+        'typing_extensions>=4.5.0',
     ]
     import pkg_resources  # noqa
 
