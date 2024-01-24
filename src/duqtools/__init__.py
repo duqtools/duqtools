edf23051d3131ce22be1307f0a2b14cc46f9bff4
Github Action (publish.yaml) <victor@lipsum.eu>
2024-01-24 08:17:37 +0000

    Bump version to: 3.0.2

--- src/duqtools/__init__.py
+++ src/duqtools/__init__.py
@@ -17,7 +17,7 @@ fix_dependencies()
 
 __author__ = 'Stef Smeets'
 __email__ = 's.smeets@esciencecenter.nl'
-__version__ = '3.0.1'
+__version__ = '3.0.2'
 
 import logging  # noqa
 import warnings  # noqa
