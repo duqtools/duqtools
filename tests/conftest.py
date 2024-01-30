c06c3f2a66b66a508b81fe8e69fde897c2deef14
Stef Smeets <s.smeets@esciencecenter.nl>
2024-01-30 11:05:22 +0100

    Set path in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 
 import pytest
@@ -7,3 +8,5 @@ import pytest
 
 def pytest_configure():
     pytest.TEST_DATA = Path(__file__).parent / 'test_data'
+
+    os.environ['JETTO_LOOKUP'] = str(pytest.TEST_DATA / 'jetto_lookup.json')
