ccf67f7803bc947ae8c8700803a63f4e23758311
Stef Smeets <s.smeets@esciencecenter.nl>
2024-09-23 16:14:24 +0200

    Update dashboard logo to use official api

--- src/duqtools/dashboard/_shared.py
+++ src/duqtools/dashboard/_shared.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import base64
 import sys
 from pathlib import Path
 from typing import Sequence
@@ -98,44 +97,7 @@ def get_dataset(handles, variable, *, include_error: bool = False):
     return _get_dataset(handles, variable, include_error=include_error)
 
 
-@st.cache_data
-def get_base64_of_bin_file(png_file):
-    with open(png_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def build_markup_for_logo(
-        png_file,
-        background_position='50% 10%',
-        margin_top='10%',
-        image_width='53%',
-        image_height='100%',
-        nav=True,  # page has navigation
-):
-    binary_string = get_base64_of_bin_file(png_file)
-
-    loc = 'stSidebarHeader' if nav else 'stSidebarUserContent'
-
-    return f"""
-            <style>
-                [data-testid="{loc}"] {{
-                    background-image: url("data:image/png;base64,{binary_string}");
-                    background-repeat: no-repeat;
-                    background-position: {background_position};
-                    margin-top: {margin_top};
-                    margin-bottom: {margin_top};
-                    background-size: {image_width} {image_height};
-                }}
-            </style>
-            """
-
-
 def add_sidebar_logo():
-    """Based on: https://stackoverflow.com/a/73278825."""
-    png_file = data_directory / 'logo.png'
-    logo_markup = build_markup_for_logo(png_file, nav=True)
-    st.markdown(
-        logo_markup,
-        unsafe_allow_html=True,
-    )
+    """https://docs.streamlit.io/develop/api-reference/media/st.logo"""
+    png_file = str(data_directory / 'logo.png')
+    st.logo(png_file, link='https://duqtools.readthedocs.org')
